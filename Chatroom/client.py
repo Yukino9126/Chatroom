854224bc9f6a67f15de73aa528d862228b35a65d
Yukino <snowlychen2384@gmail.com>
2024-03-25 00:40:18 +0800

    Client recv ok

--- Chatroom/client.py
+++ Chatroom/client.py
@@ -1,6 +1,7 @@
 import threading
 import socket
 import json
+import time
 MAXBUF = 65535
 
 class Send_Thread(threading.Thread):
@@ -16,29 +17,27 @@ class Send_Thread(threading.Thread):
                 msg = input("> ")
             except EOFError:
                 break
-            if msg == '':
-                break
-            data = bytes(json.dumps((self.name, msg)), 'UTF-8')
-            self.sock.send(data)
+            if msg != '':
+                data = bytes(json.dumps((self.name, msg)), 'UTF-8')
+                self.sock.send(data)
         self.sock.close()
 
 
-# class Recv_Thread(threading.Thread):
+class Recv_Thread(threading.Thread):
 
-#     def __init__(self, sock):
-#         threading.Thread.__init__(self)
-#         self.sock = sock
+    def __init__(self, sock):
+        threading.Thread.__init__(self)
+        self.sock = sock
 
-#     def run(self):
-#         print('recv')
-#         while True:
-#             try:
-#                 data = self.sock.recv(MAXBUF)
-#                 if data == b'':
-#                     break
-#                 print(data.decode())
-#             except EOFError:
-#                 pass
+    def run(self):
+        while True:
+            try:
+                data = self.sock.recv(MAXBUF)
+                if data == b'':
+                    break
+                print(data.decode())
+            except EOFError:
+                pass
 
 def client(host, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -46,9 +45,9 @@ def client(host, port):
     sock.connect((host, port))
 
     send = Send_Thread(sock, input('Your name? '))
-    # recv = Recv_Thread(sock)
+    recv = Recv_Thread(sock)
     send.start()
-    # recv.start()
+    recv.start()
 
 if __name__ == '__main__':
     client('127.0.0.1', 20000)
