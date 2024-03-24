e91f85b2af39ac4b5f4219c1875424407d2eea16
Yukino <snowlychen2384@gmail.com>
2024-03-24 22:46:59 +0800

    Add main.py

---
+++ Chatroom/main.py
@@ -0,0 +1,15 @@
+import argparse
+from server import server
+from client import client
+
+if __name__ == '__main__':
+    choices = {'client': client, 'server': server}
+    parser = argparse.ArgumentParser(description='Send and receive over TCP')
+    parser.add_argument('role', choices=choices, help='which role to play')
+    parser.add_argument('host', help='interface the server listens at;'
+                        ' host the client sends to')
+    parser.add_argument('-p', metavar='PORT', type=int, default=1060,
+                        help='TCP port (default 1060)')
+    args = parser.parse_args()
+    function = choices[args.role]
+    function(args.host, args.p)
