7477d37683740da09266ea2633e4e3839965db9c
Yukino <snowlychen2384@gmail.com>
2024-03-26 03:51:50 +0800

    Add slash command

--- Chatroom/server.py
+++ Chatroom/server.py
@@ -5,20 +5,34 @@ MAXBUF = 65535
 
 class Thread(threading.Thread):
 
+    users = {}
+
     def __init__(self, newsock, sockname):
         threading.Thread.__init__(self)
         self.sock = newsock
         self.sockname = sockname
+        Thread.users[sockname] = ''
 
     def run(self):
+        self.sock.settimeout(1.0)
         while True:
             try:
                 data = self.sock.recv(MAXBUF)
-                if data == b'': break
-                print(data.decode())
-            except EOFError:
-                break
+                if data == b'':
+                    break
+                name, msg = json.loads(data.decode())
+                if msg[0] == '/':
+                    if msg[1:5] == 'QUIT':
+                        print('QUIT')
+                    elif msg[1:5] == 'USER':
+                        Thread.users[self.sockname] = msg[5:].strip()
+                    elif msg[1:4] == 'WHO':
+                        print(Thread.users)
+                print(f'{name}: {msg}')
+            except socket.timeout:
+                self.sock.send(b'AAA')
         self.sock.close()
+        Thread.users.pop(self.sockname)
 
 def server(interface, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
