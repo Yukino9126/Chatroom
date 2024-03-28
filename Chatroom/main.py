758767d5337d0bff7a522dfd38755c358b5d568b
Yukino <snowlychen2384@gmail.com>
2024-03-29 05:23:16 +0800

    Modify main.py

--- Chatroom/main.py
+++ Chatroom/main.py
@@ -4,12 +4,12 @@ from client import client
 
 if __name__ == '__main__':
     choices = {'client': client, 'server': server}
-    parser = argparse.ArgumentParser(description='Send and receive over TCP')
+    parser = argparse.ArgumentParser(description='Internet Relay Chat')
     parser.add_argument('role', choices=choices, help='which role to play')
     parser.add_argument('host', help='interface the server listens at;'
                         ' host the client sends to')
-    parser.add_argument('-p', metavar='PORT', type=int, default=1060,
-                        help='TCP port (default 1060)')
+    parser.add_argument('-p', metavar='PORT', type=int, default=6667,
+                        help='IRC port (default 6667)')
     args = parser.parse_args()
     function = choices[args.role]
     function(args.host, args.p)
